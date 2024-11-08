b08cffd4886bd564f339360c88bd94ccad523809
Nehi <dawid.pospula@gmail.com>
2024-11-08 18:30:55 +0100

    working version (no cli)

---
+++ src/main/java/pl/edu/wszib/transaction/chain/generator/Transaction.java
@@ -0,0 +1,48 @@
+package pl.edu.wszib.transaction.chain.generator;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.codec.digest.DigestUtils;
+
+@Getter
+@Setter
+
+public class Transaction {
+    private String lastTransaction;
+    private double amount;
+    private int nonce;
+    private String hash;
+
+    public Transaction(String lastTransaction, double amount) {
+        this.lastTransaction = lastTransaction;
+        this.amount = amount;
+        this.nonce = this.findNonce();
+        this.hash = DigestUtils.md5Hex(this.amount + this.lastTransaction + this.nonce);
+    }
+
+    private int findNonce() {
+        nonce = 0;
+        while(!DigestUtils.md5Hex(this.amount + this.lastTransaction + this.nonce).endsWith("00000")) {
+            this.nonce++;
+        }
+        return nonce;
+    }
+
+    @Override
+    public String toString() {
+        final String SEPARATOR = ", ";
+        return new StringBuilder()
+                .append("Hash: ")
+                .append(this.getHash())
+                .append(SEPARATOR)
+                .append("Amount: ")
+                .append(this.getAmount())
+                .append(SEPARATOR)
+                .append("Nonce: ")
+                .append(this.getNonce())
+                .append(SEPARATOR)
+                .append("Last transaction: ")
+                .append(this.getLastTransaction())
+                .toString();
+    }
+}
